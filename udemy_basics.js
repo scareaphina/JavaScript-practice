a5f174820d8d4206a5fe96b3abdfd4b0d7566807
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-25 15:16:29 -0400

    Update udemy_basics.js

--- udemy_basics.js
+++ udemy_basics.js
@@ -21,3 +21,28 @@ console.log(job);
 var _3years = 3;
 var johnMark = 'John and Mark';
 var if = 23;
+
+/****************** 
+ * Variable mutation and type coercion
+*/
+
+var firstName = 'John';
+var age = 28;
+
+// Type coercion
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age =  'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last Name?');
+console.log(firstName + ' ' + lastName);
