d5691da607ae361c6d443a973a07a1d8b5c24594
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-25 15:42:41 -0400

    Update udemy_basics.js

--- udemy_basics.js
+++ udemy_basics.js
@@ -75,3 +75,32 @@ console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John.');
 var x;
 console.log(typeof x);
+
+
+var now = 2018;
+var yearJohn = 1989;
+var fullAge = 18;
+
+// Multiple operators
+var isFullAge = now - yearJohn >= fullAge; // true
+console.log(isFullAge)
+
+// Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark) / 2;
+console.log(average);
+
+// Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 + 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// More operators
+x *= 2;
+console.log(x);
+x *= 10;
+console.log(x);
+x ++;
+console.log(x);
+
