997a687802454d512d3c20701b77bab4c32240cc
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-25 15:07:35 -0400

    Create udemy_basics.js

---
+++ udemy_basics.js
@@ -0,0 +1,23 @@
+/*******************************
+* Variables and data types
+*/
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+// variable naming rules
+var _3years = 3;
+var johnMark = 'John and Mark';
+var if = 23;
