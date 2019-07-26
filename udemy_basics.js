5abc75959f13cbe50b4a8a9cc6e0c4bd92fef652
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-26 09:41:41 -0400

    Update udemy_basics.js

--- udemy_basics.js
+++ udemy_basics.js
@@ -167,3 +167,19 @@ if (MarkBMI > JohnBMI) {
     console.log('John\'s BMI is higher than mark\'s')
 }
 
+/***********************
+ * Boolean logic
+ */
+
+var firstName = 'John';
+var age = 16;
+
+if (age < 13) {
+    console.log(firstName + ' is a child.');
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager.');
+} else if (age >=  20 && age < 30){
+    console.log(firstName + ' is a young man.');
+}
+else {console.log(firstName + ' is an adult.');
+}
