b784d8fe29da73643e3b0cb622ef0da45b3bcab3
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-26 10:16:27 -0400

    Update udemy_basics.js

--- udemy_basics.js
+++ udemy_basics.js
@@ -220,3 +220,17 @@ switch (job) {
     default:
         console.log(firstName + ' does something else.');
 }
+
+switch (true){
+    case age < 13:
+        console.log(firstName + ' is a child.');
+        break;
+    case age >= 13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    case age >=  20 && age < 30:
+        console.log(firstName + ' is a young man.');
+        break;
+    default:
+        console.log(firstName + ' is an adult.');
+}
