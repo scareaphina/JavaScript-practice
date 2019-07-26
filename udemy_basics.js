b2bde067b243cf5004ee1ed4fa1623f050398ef9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-26 09:54:20 -0400

    Update udemy_basics.js

--- udemy_basics.js
+++ udemy_basics.js
@@ -183,3 +183,22 @@ if (age < 13) {
 }
 else {console.log(firstName + ' is an adult.');
 }
+
+/*********************
+ * The Ternary Operator and Switch Statements
+ */
+
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(drink);
+
+/*if(age >= 18) {
+    var drink = 'beer';
+} else {
+    var drink = 'juice';
+}*/
+
