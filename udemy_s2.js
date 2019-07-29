90c7378fc192615f4162306d830de6d79da97c23
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-07-29 14:15:47 -0400

    Update udemy_s2.js

--- udemy_s2.js
+++ udemy_s2.js
@@ -379,3 +379,34 @@ console.log(whatDoYouDo('retired', 'Mark'));
 
  var isDesigner = John.indexOf('designer') === -1 ? 'John is NOT a designer.' : 'John IS a designer.';
 console.log(isDesigner);
+
+/********************
+ * Objects and properties
+ */
+
+ // Object literal
+ var john = {
+     firstName: 'John',
+     lastName: 'Smith',
+     birthYear: 1990,
+     family: ['Jane', 'Mark', 'Bob', 'Emily'],
+     job: 'teacher',
+     isMarried: false  
+ };
+
+ console.log(john.firstName);
+ console.log(john['lastName']);
+ var x = 'birthYear';
+ console.log(john[x]);
+
+ // object mutation
+ john.job = 'designer';
+ john['isMarried'] = true;
+ console.log(john);
+
+ // new Object syntax
+ var jane = new Object();
+ jane.name = 'Jane';
+ jane.birthYear = 1969;
+ jane['lastName'] = 'Smith';
+ console.log(jane);
