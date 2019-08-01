4c647bef4f0ca6791115b2e19860ab5fb1db3191
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-01 09:15:33 -0400

    Update udemy_s2.js

--- udemy_s2.js
+++ udemy_s2.js
@@ -476,3 +476,31 @@ for (var i = 0; i < john.length; i++){
 for (var i = john.length - 1; i >= 0; i--){
     console.log(john[i]);
 }
+
+/*************
+ * Functions
+ */
+
+function calculateAge(birthYear) {
+    return 2019 - birthYear;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1948);
+var ageJane = calculateAge(1969);
+console.log(ageJohn, ageJane, ageMike);
+
+function yearsUntilRetirement(year, firstName) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(firstName + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstName + ' is already retired!');
+    }
+}
+
+yearsUntilRetirement(1990, 'John');
+yearsUntilRetirement(1948, 'Mike');
+yearsUntilRetirement(1969, 'Jane');
