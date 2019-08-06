6f335116eba84c0ac3fb1e4ba59a18d51882df1c
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-06 11:59:23 -0400

    Update Challenge6.js

--- Exercises/Challenge6.js
+++ Exercises/Challenge6.js
@@ -7,17 +7,6 @@
  * 3) Add another die to the game, so that there are two die now. The player loses his current score when one of them is a 1. (HINT: you will need CSS to position the second die, so take a look at the CSS code for the first one)
  */
 
-/*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
-*/
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
