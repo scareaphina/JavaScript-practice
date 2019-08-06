8f60a33aecd997ac8d8b726162e77dac3074565a
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-06 14:39:37 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -53,3 +53,23 @@ console.log(safi);
 console.log(cole);
 console.log(briar);
 console.log(claire);
+
+// object.create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,
+    {
+        name: { value: 'Jane' },
+        yearOfBirth: { value: 1969 },
+        job: { value: 'designer' }
+    });
