79d8660c8eb0a767e737dee55562ec4d4704f8f9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-06 14:11:38 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -12,4 +12,15 @@ var Person = function(name, yearOfBirth, job) {
     this.job = job;
 }
 
+Person.prototype.calculateAge = function () {
+    console.log(2016 - this.yearOfBirth);
+};
+
+// new creates an empty object, then the constructor function (person) is called and the arguments are added to the empty object
 var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1948, 'retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
