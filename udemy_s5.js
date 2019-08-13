0015bdcb87199181071df07d0c5af25d5620ceee
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-13 15:04:20 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -335,3 +335,31 @@ var emilyFormal = john.presentation.bind(emily, 'formal');
 
 emilyFormal('afternoon');
 emilyFormal('evening');
+
+// another bind example
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(limit, el) {
+    return el>= limit;
+}
+
+// can bind the arraycalc to the limit of full age for Japan, which is 20. change the isFullAge function to take a limit rather than the number 18, then bind the isFullAge function
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+
+console.log(ages);
+console.log(fullJapan);
