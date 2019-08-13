df3d72cd461f198fcb146e324093f7b7a3114f20
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-13 11:21:21 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -157,3 +157,19 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please xplain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('Hello ' + name + ', What do you do?')
+        }
+    }
+}
