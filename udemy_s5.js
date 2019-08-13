c4dba71a1ea5057dc2be56a6f0146f0959edf7d1
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-13 14:05:47 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -280,3 +280,21 @@ retirementIceland(1990);
 
 
 //retirement(66)(1990);
+
+///////////////////
+// the interview questions function with closure use
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please xplain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {console.log('Hello ' + name + ', What do you do?')
+        }
+    }
+}
+
+interviewQuestion('teacher')('John');
+interviewQuestion('designer')('Jane');
+interviewQuestion('driver')('Mike');
