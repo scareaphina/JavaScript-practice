1a675e3c5a87e950cbd3f747f84f4a5462b80f7d
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-13 13:47:18 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -259,3 +259,24 @@ var retirementUS = retirement(66);
 retirementUS(1990);
 
 retirement(66)(1990);
+
+/////////////////////////
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementGermany(1990);
+retirementUS(1990);
+retirementIceland(1990);
+
+
+//retirement(66)(1990);
