eab25a4be410bed37c840c63a9d4db4465e98e68
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-08-13 16:05:23 -0400

    Update Challenge7.js

--- Exercises/Challenge7.js
+++ Exercises/Challenge7.js
@@ -20,3 +20,14 @@ c) correct answer (I would use a number for this)
 6. Check if the answer is correct and print to the console whether the answer is correct or not (HINT: write another method for this).
 
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (HINT: we learned a special technique to do exactly that)
+*/
+
+function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+var ql = new Question('Is Seraphina queer?',
+                        ['Yes', 'No'], 
+                        0);
