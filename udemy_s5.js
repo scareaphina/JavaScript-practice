10af69a12fb9cdd738068c95ec93c751f86280a9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-23 16:09:04 -0400

    Update udemy_s5.js

--- udemy_s5.js
+++ udemy_s5.js
@@ -319,6 +319,7 @@ retirementUS(1990);
 retirement(66)(1990);
 
 /////////////////////////
+// closures
 
 function retirement(retirementAge) {
     var a = ' years left until retirement.';
@@ -328,13 +329,19 @@ function retirement(retirementAge) {
     }
 }
 
+// inner function is able to use the retirement and age function that are already gone (have already returned), and that is the function of the closure
+
 var retirementUS = retirement(66);
 var retirementGermany = retirement(65);
 var retirementIceland = retirement(67);
 
 retirementGermany(1990);
-retirementUS(1990);
 retirementIceland(1990);
+retirementUS(1990);
+retirement(66)(1990);
+
+
+//closures:  an inner function always has access to the variables and parameters of its outer function, even after the outer function has returned
 
 
 //retirement(66)(1990);
