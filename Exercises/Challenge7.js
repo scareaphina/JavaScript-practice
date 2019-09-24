6c25c7005c89a25aab76b846f21968fdb6465c02
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-24 11:21:09 -0400

    Update Challenge7.js

--- Exercises/Challenge7.js
+++ Exercises/Challenge7.js
@@ -1,33 +1,34 @@
 ////////////////////////
-// Coding Challenge
+// Coding Challenge 7
 
 /*
 --- Let's build a fun quiz game in the console! ---
-
 1. Build a function constructor called Question to describe a question. A question shold include:
 a) question itself
 b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
 c) correct answer (I would use a number for this)
-
 2. Create a couple of questions using the constructor
-
 3. Store them all inside an array
-
 4. Select one random question and log it on the console, together with the possible answers (each question should ahve a number) (HINT: write a method for the Questions objects for this task).
-
 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
-
 6. Check if the answer is correct and print to the console whether the answer is correct or not (HINT: write another method for this).
-
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (HINT: we learned a special technique to do exactly that)
+
+--- Expert Level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends, so include an option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add one point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable with at this point).
+11. Display the score in the console. Use yet another method for this.
 */
 
 function Question(question, answers, correct) {
     this.question = question;
     this.answers = answers;
-    this.correct = correct;
+    this.correctAnswer = correct;
 }
 
-var ql = new Question('Is Seraphina queer?',
-                        ['Yes', 'No'], 
-                        0);
+var q1 = new Question('Is Seraphina queer?', ['Yes', 'No'], 0);
+var q2 = new Question('How many cats does Seraphina have?', [0, 1, 2, 3, 4, 5], 2);
+var q3 = new Question('Does Seraphina love coding?', ['Yes', 'No'], 0);
+var q4 = new Question('Is Seraphina a gamer?', ['Yes', 'No'], 0);
