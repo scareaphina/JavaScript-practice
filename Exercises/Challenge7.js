992c1305835f1117598f4baebb72a78f5e576af8
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-24 12:00:02 -0400

    Update Challenge7.js

--- Exercises/Challenge7.js
+++ Exercises/Challenge7.js
@@ -13,13 +13,6 @@ c) correct answer (I would use a number for this)
 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
 6. Check if the answer is correct and print to the console whether the answer is correct or not (HINT: write another method for this).
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (HINT: we learned a special technique to do exactly that)
-
---- Expert Level ---
-
-8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
-9. Be careful: after Task 8, the game literally never ends, so include an option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
-10. Track the user's score to make the game more fun! So each time an answer is correct, add one point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable with at this point).
-11. Display the score in the console. Use yet another method for this.
 */
 
 (function () {
@@ -60,3 +53,12 @@ var answer = parseInt(prompt('Please select the correct answer.'));
 
 questions[n].checkAnswer(answer);
 })();
+
+/*
+--- Expert Level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends, so include an option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add one point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable with at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
