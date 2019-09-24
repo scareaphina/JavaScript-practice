02bf384895f60cf5e8337eba5de604ecc5b831d9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-24 12:08:05 -0400

    Update Challenge7.js

--- Exercises/Challenge7.js
+++ Exercises/Challenge7.js
@@ -15,7 +15,6 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (HINT: we learned a special technique to do exactly that)
 */
 
-(function () {
 function Question(question, answers, correct) {
     this.question = question;
     this.answers = answers;
@@ -52,49 +51,84 @@ questions[n].displayQuestion();
 var answer = parseInt(prompt('Please select the correct answer.'));
 
 questions[n].checkAnswer(answer);
-})();
+
+/*
+--- Expert Level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends, so include an option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add one point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable with at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
 
 (function () {
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-Question.prototype.displayQuestion = function() {
-    console.log(this.question);
-    for (var i = 0; i < this.answers.length; i++) {
-        console.log(i + ': ' +
-            this.answers[i]);
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
     }
-}
 
-Question.prototype.checkAnswer = function(ans) {
-    if (ans === this.correct) {
-        console.log('Correct answer!');
-    } else {
-        console.log('Wrong answer, try again!');
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' +
+                this.answers[i]);
+        }
     }
-}
 
-var q1 = new Question('Is Seraphina queer?', ['Yes', 'No'], 0);
-var q2 = new Question('How many cats does Seraphina have?', [0, 1, 2, 3, 4, 5], 2);
-var q3 = new Question('Does Seraphina love coding?', ['Yes', 'No'], 0);
-var q4 = new Question('Is Seraphina a gamer?', ['Yes', 'No'], 0);
+    Question.prototype.checkAnswer = function (ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer, try again!');
+        }
+    }
 
-var questions = [q1, q2, q3, q4];
-
-function nextQuestion() {
-    var n = Math.floor(Math.random() * questions.length);
-    questions[n].displayQuestion();
+    var q1 = new Question('Is Seraphina queer?', ['Yes', 'No'], 0);
     
-    var answer = parseInt(prompt('Please select the correct answer.'));
+    var q2 = new Question('How many cats does Seraphina have?', [0, 1, 2, 3, 4, 5], 2);
     
-    questions[n].checkAnswer(answer);
+    var q3 = new Question('Does Seraphina love coding?', ['Yes', 'No'], 0);
+    
+    var q4 = new Question('Is Seraphina a gamer?', ['Yes', 'No'], 0);
+
+    var questions = [q1, q2, q3, q4];
+
+    function nextQuestion() {
+        var n = Math.floor(Math.random() * questions.length);
+        questions[n].displayQuestion();
+        
+        var answer = prompt('Please select the correct answer.');
+        
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer));
+            nextQuestion();
+        }
+    }
 
     nextQuestion();
-}
-
-nextQuestion();
-
+    
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
