488bd0508818cc1d20c786e29ed533d9701d9076
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-25 11:00:17 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -37,16 +37,30 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    document.querySelector('.add__btn').addEventListener('click', function() {
-        
+    var ctrlAddItem = function() {
+
         // 1. get field input data
+
         // 2. add item to the budget controller
+        
         // 3. add new item to UI
+        
         // 4. calculate the budget
+        
         // 5. display the budget on the UI
 
+        console.log('It works.');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
     });
-
-
 
 })(budgetController, UIController);
