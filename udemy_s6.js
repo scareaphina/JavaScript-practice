01c7c1af9673a4383786e1ae26466de35b5e8d8c
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-25 12:31:49 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -30,13 +30,24 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
     return {
         getInput: function() {
             return {
-            type: document.querySelector('.add__type').value, // will be either inc or exp
-            description: document.querySelector('.add__description').value,
-            value: document.querySelector('.add__value').value
+            type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     };
 })();
@@ -45,10 +56,12 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. get field input data
-        var input = UICtrl.getinput();
+        var input = UICtrl.getInput();
         console.log(input);
 
         // 2. add item to the budget controller
@@ -61,7 +74,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         
