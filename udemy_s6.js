053192ac9bd2b5613763ac7067de277a9c9f8da9
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-25 09:59:17 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -4,3 +4,27 @@
 // encapsulate some data into privacy and expose other data publicly
 
 // separate out into ui and data modules
+
+///////////////////////////////
+
+// this section contains:
+// how to use the module pattern
+// private and public data, encapsulation, and separation of concerns
+
+///////////////////////////////
+
+var budgetController = (function() {
+    
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+
+})();
