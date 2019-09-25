1a06bd2f3a88ae4623b50f2a33a51e485d7f2912
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-25 09:43:10 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -1,1 +1,6 @@
+// structuring our code with modules
+// important aspect of any robust application articitecture
+// keep unites of code for a project cleanly separated and organized
+// encapsulate some data into privacy and expose other data publicly
 
+// separate out into ui and data modules
