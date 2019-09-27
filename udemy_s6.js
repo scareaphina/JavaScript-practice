ccb0487ad64c912c929e19364915757b37ee8cf8
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-27 09:56:50 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -151,7 +151,11 @@ var UIController = (function () {
         inputValue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        expensesContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expensesLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -202,6 +206,15 @@ var UIController = (function () {
 
         },
 
+        displayBudget: function (obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+            document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -234,7 +247,8 @@ var controller = (function (budgetCtrl, UICtrl) {
         var budget = budgetCtrl.getBudget();
 
         // 3. display the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
+
     };
 
     var ctrlAddItem = function () {
