475299a17d7132da0e6e404b68b47743dc167961
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-09-27 11:29:14 -0400

    Update udemy_s6.js

--- udemy_s6.js
+++ udemy_s6.js
@@ -215,7 +215,7 @@ var UIController = (function () {
             if (obj.percentage > 0) {
                 document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
             } else {
-                document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+                document.querySelector(DOMstrings.percentageLabel).textContent = '--';
             }
             
         },
@@ -281,7 +281,13 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     return {
         init: function () {  // public initiation function
-            console.log('Application has started.')
+            console.log('Application has started.');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
             setupEventListeners();
         }
     };
