788cd0bcf1ae3515db0747d5d99545bc5635269c
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-21 10:16:04 -0400

    Update practice.js

--- udemy/practice.js
+++ udemy/practice.js
@@ -135,3 +135,25 @@ if (guestList.includes(maybeGuest)) {
 } else {
   alert("Sorry " + guestName + ", you aren't on the list.");
 }
+
+// fizzbuzz
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+
+if (count % 3 === 0 && count % 5 === 0) {
+  output.push("FIZZBUZZ");
+} else if (count % 3 === 0) {
+  output.push("FIZZ");
+} else if (count % 5 === 0) {
+  output.push("BUZZ");
+} else {
+  output.push(count);
+}
+
+count++;
+console.log(output);
+
+}
