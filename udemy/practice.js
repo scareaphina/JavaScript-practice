6912f7226a1878862eec7a9dc9dc2c23e6aca311
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-22 11:35:47 -0400

    Update practice.js

--- udemy/practice.js
+++ udemy/practice.js
@@ -160,6 +160,29 @@ console.log(output);
 }
 }
 
+// fizzbuzz with for loop
+
+var output = [];
+
+function fizzBuzz() {
+
+for(var count = 1; count < 100; count++) {
+
+if (count % 3 === 0 && count % 5 === 0) {
+  output.push("FIZZBUZZ");
+} else if (count % 3 === 0) {
+  output.push("FIZZ");
+} else if (count % 5 === 0) {
+  output.push("BUZZ");
+} else {
+  output.push(count);
+}
+
+count++;
+console.log(output);
+}
+}
+
 // bottles of beer
 
 var numberOfBottles = 99
