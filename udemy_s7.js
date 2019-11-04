87b5e2156b8a3a72042ccb674d8f0aa13f1f4820
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-04 13:47:14 -0500

    Update udemy_s7.js

--- udemy_s7.js
+++ udemy_s7.js
@@ -109,10 +109,17 @@ console.log(ages5);
 
 
 // ES6
-const ages6 = years.map(el => 2019 - el);
+let ages6 = years.map(el => 2019 - el);
 console.log(ages6);
 
 ages6 = years.map((el, index) => `Age element ${index + 1}: ${2019 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+  return `Age element ${index + 1}: ${age}.`
+});
 console.log(ages6);
 
 // deconstructing
