f7b62583178b3324db9df98f7641dd40ed92280e
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-04 10:15:11 -0500

    Update udemy_s7.js

--- udemy_s7.js
+++ udemy_s7.js
@@ -1,1 +1,27 @@
+// new features
 
+// variable declarations with let and const
+
+// ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+// ES6
+const name6 = 'Jane Smith'; // variables declared with const are immutable
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+
+// blocks and IIFEs
+// strings
+// arrow functions
+// deconstructing
+// arrays
+// the spread operator
+// rest and default parameters
+// maps
+// classes and subclasses
+// promises
+// native modules
