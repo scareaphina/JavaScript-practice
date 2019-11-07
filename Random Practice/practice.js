80c0317be8c6d508490cd3de9ad9a503b06b3a4a
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-07 14:07:54 -0500

    Update practice.js

--- Random Practice/practice.js
+++ Random Practice/practice.js
@@ -66,6 +66,13 @@ const person = {
 for (let key in person)
 console.log(key, person[key]);
 
+
+
+const colors = ["red", "green", "blue"];
+
+for (let index in colors)
+console.log(index, colors[index]);
+
 // // dot notation
 // person.name
 //
