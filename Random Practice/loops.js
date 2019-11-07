0dff9dc99ecf6595758be4d10b9ad8d1172950f1
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-07 12:41:30 -0500

    Add files via upload

---
+++ Random Practice/loops.js
@@ -0,0 +1,12 @@
+// for loops
+
+// for (initial expression, condition, increment expresion);
+// executes as long as the condition is true
+
+for (let i = 0; i <= 5; i++) {
+  console.log("Hello World!", i);
+}
+
+for (let i = 5; i >= 1; i--) {
+  if (i % 2 !== 0) console.log(i);
+}
