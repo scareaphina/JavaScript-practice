d66d3ad773c31a73fc4d8550ba70a2da26a3f174
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2020-01-07 14:24:42 -0500

    Update notes.js

--- The Good Parts/notes.js
+++ The Good Parts/notes.js
@@ -135,3 +135,27 @@ console.log(another_stooge.nickame);
 delete another_stooge.nickname;
 
 console.log(another_stooge.nickame);
+
+// Global Abatement
+
+var MYAPP = {};
+
+MYAPP.stooge = {
+  "first-name": "Joe",
+  "last-name": "Howard"
+};
+
+MYAPP.flight = {
+  airline: "Oceanic",
+  number: 815,
+  departure: {
+    IATA: "SYD",
+    time: "2004-09-22 14:55",
+    city: "Sydney"
+  },
+  arrival: {
+    IATA: "LAX",
+    time: "2004-09-23 10:42",
+    city: "Los Angeles"
+  }
+};
