e2e9c8e71b5237406d6c3bfdd13e2dd74d4bd85f
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2020-01-17 12:43:30 -0500

    Update game.js

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -1,17 +1,23 @@
-var buttonColors = ["red", "blue", "green", "yellow"];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
-var randomChosenColor = buttonColors[randomNumber];
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   
   gamePattern.push(randomChosenColour);
-
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
   
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
 }
