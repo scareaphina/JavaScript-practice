c4302aaf31db00160893da9c8e8549416b91410c
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2020-01-28 10:23:47 -0500

    Update game.js

--- Simon_Game/game.js
+++ Simon_Game/game.js
@@ -4,24 +4,39 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 
+var started = false;
+
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
 
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
-
   animatePress(userChosenColour);
 });
 
 function nextSequence() {
+
+  level++;
+
+  $("#level-title").text("Level " + level);
 
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
 }
 
@@ -31,9 +46,7 @@ function playSound(name) {
 }
 
 function animatePress(currentColor) {
-
   $("#" + currentColor).addClass("pressed");
-
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
